70dbfb5f2cbcc4b0b8f2d75774612eac9b3d6e0c
s19003 <s19003@std.it-college.ac.jp>
2021-11-23 17:21:56 +0900

    11/23

--- Youtube/youtube.js
+++ Youtube/youtube.js
@@ -4,11 +4,12 @@ const key = 'AIzaSyDh7tJHG-beT2-gFNpJGIvnmVsitnQvSiY'
 class Youtube {
   constructor() {
     this.id = ''
-    this.chatId = ''
+    this.activeId = ''
+    this.nextId = ''
   }
 
   getChatId = async (id = '') => {
-    let chatId = ''
+    let chats
     let endpointUrl = 'https://www.googleapis.com/youtube/v3/videos'
     endpointUrl += `?key=${key}`
     endpointUrl += `&id=${id}`
@@ -23,23 +24,44 @@ class Youtube {
         try {
           const activeLiveChatId = res.body['items'][0]['liveStreamingDetails']['activeLiveChatId']
           if (activeLiveChatId !== undefined) {
-            this.chatId = activeLiveChatId
+            const chatMessages = await this.getChatMessages(activeLiveChatId)
+            this.nextId = chatMessages['nextPageToken']
           }
           break
         } catch {
           break
         }
       case 1:
+        const response = await needle('get', endpointUrl)
+        try {
+          const activeLiveChatId =
+            response.body['items'][0]['liveStreamingDetails']['activeLiveChatId']
+          if (activeLiveChatId !== undefined) {
+            chats = await this.getChatMessages(activeLiveChatId, this.nextId)
+            this.nextId = chats['nextPageToken']
+          }
+          break
+        } catch {
+          break
+        }
         break
     }
 
-    return chatId
+    return chats
+  }
+
+  getChatMessages = async (activeLiveChatId, nextId = '') => {
+    let endpointUrl = 'https://www.googleapis.com/youtube/v3/liveChat/messages'
+    endpointUrl += `?liveChatId=${activeLiveChatId}`
+    endpointUrl += `&part=snippet`
+    endpointUrl += `&key=${key}`
+
+    if (nextId != '') endpointUrl += `&pageToken=${nextId}`
 
     const res = await needle('get', endpointUrl)
+
     if (res.body) {
       return res.body
-    } else {
-      throw new Error('リクエストに失敗しました。')
     }
   }
 
@@ -47,12 +69,14 @@ class Youtube {
     if (id == '') {
       this.id = ''
       this.chatId = ''
+      this.nextId = ''
       return -1
     }
 
     if (this.id == '') {
       this.id = id
       this.chatId = ''
+      this.nextId = ''
       return 0
     }
 
@@ -63,6 +87,7 @@ class Youtube {
     if (this.id != id) {
       this.id = id
       this.chatId = ''
+      this.nextId = ''
       return 0
     }
   }
